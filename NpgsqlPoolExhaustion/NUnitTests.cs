755a0751f93162695b076daf2e58d41bedcc4df5
James Kelly <james.kelly.99@gmail.com>
2016-10-26 22:34:57 +1100

    remove cruft cases

--- NpgsqlPoolExhaustion/NUnitTests.cs
+++ NpgsqlPoolExhaustion/NUnitTests.cs
@@ -7,7 +7,7 @@ using NUnit.Framework;
 
 namespace NpgsqlPoolExhaustion
 {
-    [TestFixture]
+    [TestFixture(Category ="Postgresql")]
     public class NUnitTests
     {
         private string _tableName;
@@ -25,14 +25,6 @@ namespace NpgsqlPoolExhaustion
             await db.DropTable(_tableName);
         }
 
-        //[TestCase(98,"server=localhost;port=5433;database=rebus2_test;user id=test;password=test")]
-        //[TestCase(99,"server=localhost;port=5433;database=rebus2_test;user id=test;password=test;")]
-        //[TestCase(10000,"server=localhost;port=5433;database=rebus2_test;user id=test;password=test;Maximum Pool Size=100000")]
-        //[TestCase(98,"server=localhost;port=5433;database=rebus2_test;user id=test;password=test;Maximum Pool Size=1000; Connection Idle Lifetime=1; Connection Pruning Interval 0.5;")]
- //       [TestCase(200000,"server=localhost;port=5433;database=rebus2_test;user id=test;password=test; maximum pool size=30;")]
-//        [TestCase(101,"server=localhost;port=5433;database=rebus2_test;user id=test;password=test; maximum pool size=1;")]
-        [TestCase(200000,"server=localhost;port=5433;database=rebus2_test;user id=test;password=test; maximum pool size=30;")]
-        [TestCase(200001,"server=localhost;port=5433;database=rebus2_test;user id=test;password=test; maximum pool size=30;")]
         [TestCase(500,"server=localhost;port=5433;database=rebus2_test;user id=test;password=test;")]
         public async Task SaveABunchOfData(int messageCount, string connectionString)
         {
